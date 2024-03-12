8e258dc078ec22bd6cc727a754b08956468862cf
Alex Tsurikov <alextsurikman@gmail.com>
2024-03-13 00:20:12 +0300

    Добавьте файлы проекта.

---
+++ Helpers.h
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int summ(int a, int b)
+{
+	
+	return (a + b) * (a + b);
+
+}
---
+++ Lesson13.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "Helpers.h"
+
+
+
+int main()
+{
+    std::cout << "If u want to count (A+B)*(A+B)\n";
+    int a, b;
+    std::cout << "Insert A:\nA=";
+    std::cin >> a;
+    std::cout << "Insert B:\nB=";
+    std::cin >> b;
+
+    
+    int result = summ(a=a,b=b);
+    std::cout << "Result is " << result << std::endl;
+}
+
